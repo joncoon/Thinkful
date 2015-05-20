a8b21d8f4f715c8945e47431e2206c2b1dda0555
Jon Coon <jon@joncoon.com>
2015-05-19 20:24:09 -0700

    Lending club and probability stuff

---
+++ prob.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+x = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
+plt.boxplot(x)
+plt.show()
+plt.hist(x, histtype='bar')
+plt.show()
+
+import collections
+
+testlist = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
+
+x = collections.Counter(testlist)
+
+print x
+
+# calculate the number of instances in the list
+count_sum = sum(x.values())
+
+for k,v in x.iteritems():
+  print "The frequency of number " + str(k) + " is " + str(float(v) / count_sum)
+
+
+import numpy as np 
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+plt.figure()
+test_data = np.random.normal(size=1000)   
+graph1 = stats.probplot(test_data, dist="norm", plot=plt)
+plt.show() #this will generate the first graph
+plt.figure()
+test_data2 = np.random.uniform(size=1000)   
+graph2 = stats.probplot(test_data2, dist="norm", plot=plt)
+plt.show() #this will generate the second graph
