8097d7a4093519d60ea6db103684ace74edcaf00
Jon Coon <jon@joncoon.com>
2015-08-04 20:41:34 -0700

    New Cloudy Data

--- cloudydata.py
+++ cloudydata.py
@@ -8,7 +8,7 @@ apikey = '2e2f577ca9f8eb244e19aa4594cc1744/'
 
 url = 'https://api.forecast.io/forecast/' + apikey
 
-cities = { "San Francisco": '37.727239,-123.032229',
+cities = { "San_Francisco": '37.727239,-123.032229',
 			"Seattle": '47.620499,-122.350876',
 			"Philadelphia": '40.009376,-75.133346',
 			"Denver": '39.761850,-104.881105',
@@ -28,11 +28,10 @@ cur = con.cursor()
 cities.keys()
 with con:
 	cur.execute("DROP TABLE IF EXISTS daily_temp")
-	cur.execute('CREATE TABLE daily_temp ( day_of_reading INT, San Francisco REAL, Seattle REAL, Philadelphia REAL, Denver REAL, Miami REAL)')
+	cur.execute('CREATE TABLE daily_temp ( day_of_reading INT, San_Francisco REAL, Seattle REAL, Philadelphia REAL, Denver REAL, Miami REAL)')
 
 query_date = end_date - datetime.timedelta(days=30) #the current value being processed
 
-with con:
     while query_date < end_date:
         cur.execute("INSERT INTO daily_temp(day_of_reading) VALUES (?)", (int(query_date.strftime('%s')),))
         query_date += datetime.timedelta(days=1)
@@ -40,10 +39,8 @@ with con:
 for k,v in cities.iteritems():
     query_date = end_date - datetime.timedelta(days=30) #set value each time through the loop of cities
     while query_date < end_date:
-        #query for the value
-        r = requests.get(url + v + ',' +  query_date.strftime('%Y-%m-%dT12:00:00'))
-
-        with con:
+        	#query for the value
+        	r = requests.get(url + v + ',' +  query_date.strftime('%Y-%m-%dT12:00:00'))
             #insert the temperature max to the database
             cur.execute('UPDATE daily_temp SET ' + k + ' = ' + str(r.json()['daily']['data'][0]['temperatureMax']) + ' WHERE day_of_reading = ' + query_date.strftime('%s'))
 
